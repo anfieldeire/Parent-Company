5df62c97504e5e2e75ec8a7a5c46aa3f51e5d606
anfieldeire <gareth.hasson@gmail.com>
2025-06-12 11:37:55 -0400

    Add files via upload

--- Parent_Company.js
+++ Parent_Company.js
@@ -1,16 +1,9 @@
 // When the parent company field on the nuvolo company form changes, a checkbox update_data is checked.  (via business rules)
-
 // This is then used to flag companies and parent_data that need to have the parent company field on clinical models and clinical devices updated
-
 // Data filter: companies of type manufacturer. Customer req = active or inactive it doesnt matter
-
- 
-
 // Data structure: parent instance is a dict containing each set of company data, parent_data is the array that holds all the records, each record in its own dict
-
 // Once the data in the above two tables is updated, the company records are unchecked again
-
-// Main function calls the other funtcions
+// Main function calls the other functions
 
  
 
@@ -25,68 +18,33 @@ function get_companies() {
     var parent_data = [];
 
    
-
     var grc = new GlideRecord('x_nuvo_eam_company');
-
     grc.addQuery('u_parent_company', '!=', '');
-
     grc.addQuery('manufacturer', true);
-
     grc.addQuery('u_update_data', true);
-
     grc.query();
-
- 
-
+	
     while (grc.next()){
 
  
-
         var parent_instance = {};
 
- 
-
-        //global.JSUtil.logObject(parent_instance, ' NUV GH: parent instance start');
-
- 
-
         var parent_name = grc.getDisplayValue('u_parent_company');
-
         var company_sysid = grc.getValue('sys_id');
 
  
-
         parent_instance = { "company_sysid": company_sysid, "parent_name": parent_name};
-
-        //global.JSUtil.logObject(parent_instance, ' NUV GH: parent instance dict');
-
- 
-
-       
+		
 
         parent_data.push(parent_instance);
-
- 
 
        
 
     }
 
- 
-
-    //global.JSUtil.logObject(parent_data, 'Nuv GH: parent data1:' );
-
- 
-
 return parent_data; // an array containing multiple dicts
 
 }
-
- 
-
-//get_companies();
-
-
 
 
 function update_models(parent_data) {
@@ -96,52 +54,26 @@ function update_models(parent_data) {
     // updates populates the clinical model parent_company field based on a match with manufacturer  = company
 
  
-
-   
-
     for (i=0; i< parent_data.length; i++){
 
  
 
-        //global.JSUtil.logObject(parent_data[i].company_sysid, ' NUV GH: parent data dict0 ' );
-
- 
-
         var grc = new GlideRecord('x_nuvo_eam_clinical_models');
-
         grc.addQuery('manufacturer', parent_data[i].company_sysid );
-
         grc.query();
-
+		
         while (grc.next()){
 
- 
-
-                //global.JSUtil.logObject(parent_data[i].parent_name, ' NUV GH: parent data dict ' );
-
- 
-
                 grc.u_parent_company = parent_data[i].parent_name;
-
                 grc.update();
-
- 
 
         }
 
-        global.JSUtil.logObject(parent_data[i].parent_name, ' NUV GH: update complete for parent name ' );
-
- 
-
     }
 
- 
 
 }
 
-//update_models();
-
- 
 
 function update_devices(parent_data){
 
@@ -151,47 +83,25 @@ function update_devices(parent_data){
 
  
 
-    //global.JSUtil.logObject(parent_data, ' Nuv GH: update devices: arr all ' );
-
- 
-
     for (i=0; i< parent_data.length; i++){
 
  
-
         var grd = new GlideRecord('x_nuvo_eam_clinical_devices');
-
         grd.addQuery('asset_manufacturer', parent_data[i].company_sysid );
-
         grd.query();
-
-        //gs.info('NUV GH: row count: ' + grd.getRowCount());
-
- 
 
         while (grd.next()) {
 
  
-
-            //grd.u_parent_company = '';
-
             grd.u_parent_company = parent_data[i].parent_name;
-
             grd.update();
 
- 
-
             }  
-
- 
 
     }
 
 }
 
-//update_devices();
-
- 
 
 function uncheck_companies(parent_data){
 
@@ -201,39 +111,26 @@ function uncheck_companies(parent_data){
 
  
 
-   
-
     for (i=0; i< parent_data.length; i++){
 
  
-
         var grc = new GlideRecord('x_nuvo_eam_company');
-
         grc.addQuery('sys_id', parent_data[i].company_sysid);
-
         grc.query();
-
- 
 
             while (grc.next()){
 
  
-
                 grc.u_update_data = false;
-
                 grc.update();
 
- 
 
             }
 
     }      
 
  
-
 }
-
-
 
 
 function main() {
@@ -244,19 +141,11 @@ function main() {
 
  
 
-   
-
     var parent_data = get_companies();
-
- 
 
     update_models(parent_data);
 
- 
-
     update_devices(parent_data);
-
- 
 
     uncheck_companies(parent_data);
 
